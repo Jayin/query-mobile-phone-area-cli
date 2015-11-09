aee1a553a02d20ed095d403fafbf6c468f6a8180
Jayin Ton <tonjayin@gmail.com>
2015-11-10 00:48:48 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## query-mobile-phone-area-cli
+
+
+#### Install
+
+```shell
+npm install -g query-mobile-phone-area-cli
+```
+
+### Usage
+
+```shell
+mobilephone [mobilephone]
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+'use strict'
+
+var query = require('query-mobile-phone-area')
+
+if (process.argv.length < 3) {
+    console.log('请输入手机号码')
+    process.exit(0)
+} else {
+    var result = query(process.argv[2])
+    if(result){
+        console.log('省 份 : ' + result.province)
+        console.log('城 市 : ' + result.city)
+        console.log('运营商:' + result.type)
+    }else{
+        console.log('无法查询')
+    }
+}
